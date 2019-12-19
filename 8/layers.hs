8de969d0628be487f9f91bf5444d707a64a2d628
Jim McStanton <jim@jhmcstanton.com>
2019-12-19 11:57:06 -0600

    Day 8 part 2

--- 8/layers.hs
+++ 8/layers.hs
@@ -1,11 +1,13 @@
 import Data.Char (isSpace)
-import Data.List (sortBy)
+import Data.Foldable (fold)
+import Data.List (any, sortBy)
 import Control.Monad
 
 main = do
   nums <- takeWhile (not . isSpace) <$> readFile "in.txt"
   let layers = filter (\xs -> length xs > 0) . chunks (25 * 6) . fmap (\c -> read [c]) $ nums
   putStrLn . show . layerChecksum $ layers
+  putStrLn . toStr . visibleLayer $ layers
 
 chunks :: Int -> [a] -> [[a]]
 chunks n xs = chunks' n [] xs where
@@ -23,3 +25,19 @@ layerChecksum = check . head . sortBy compareLayers where
   countNums xs = (countx 1 xs, countx 2 xs)
   countx x = length . filter (== x)
   check = uncurry (*) . countNums
+
+-- Part 2
+visibleLayer :: [[Int]] -> [Int]
+visibleLayer (x:xs) = top x xs where
+  top layer []     = layer
+  top layer (x:xs) | any (== 0) layer = top (zipWith (\l x -> if l == 2 then x else l) layer x) xs
+                   | otherwise        = layer
+
+lineToStr :: [Int] -> String
+lineToStr []       = []
+lineToStr (0 : xs) = ' ' : lineToStr xs
+lineToStr (1 : xs) = 'X' : lineToStr xs
+lineToStr (2 : xs) = ' ' : lineToStr xs
+
+toStr :: [Int] -> String
+toStr = fold . fmap (\l -> lineToStr l ++ "\n") . chunks 25
