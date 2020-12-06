937fae8c409e03241827ad6140fdf332cb9f412b
Jim McStanton <jim@jhmcstanton.com>
2020-12-06 14:56:39 -0600

    Day 6 complete

--- 2020/6/solve.hs
+++ 2020/6/solve.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE LambdaCase #-}
 import qualified Data.Map.Strict    as Map
 import qualified Data.Set           as Set
 import           System.Environment        (getArgs)
@@ -8,17 +7,22 @@ main = do
   contents <- readFile (head args)
   let answers = split contents
   print . solve1 $ answers
+  print . solve2 $ answers
 
-type Answers = [Char]
+type Answers = [[Char]]
 
 solve1 :: [Answers] -> Int
-solve1 = sum . fmap (Set.size . Set.fromList)
+solve1 = sum . fmap (Set.size . Set.fromList . concat)
 
 solve2 :: [Answers] -> Int
-solve2 = undefined
+solve2 = sum . fmap allAnsweredCount where
+  allAnsweredCount :: Answers -> Int
+  allAnsweredCount a = Map.size . Map.filter (==groupSize) $ summed where
+    groupSize = length a
+    summed = foldr (\c m -> Map.insertWith (+) c 1 m) Map.empty $ concat a
 
 split :: String -> [Answers]
-split = foldr join [""] . lines where
-  join :: String -> [String] -> [String]
-  join ""     xs  = "" : xs
-  join s (x : xs) = (s ++ x) : xs
+split = foldr join [[]] . lines where
+  join :: String -> [[String]] -> [[String]]
+  join ""     xs  = [] : xs
+  join s (x : xs) = (s : x) : xs
