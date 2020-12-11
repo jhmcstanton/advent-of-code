ca54eac50742b4929ac3aea87380b18c890195d1
Jim McStanton <jim@jhmcstanton.com>
2020-12-11 09:12:39 -0600

    Slow part 2

--- 2020/10/solve.hs
+++ 2020/10/solve.hs
@@ -1,16 +1,26 @@
-import           Data.List                 (sort)
+import           Data.List                 (sort, tails)
 import qualified Data.IntMap.Strict as Map
 import           System.Environment        (getArgs)
 
 main = do
   args     <- getArgs
   contents <- readFile (head args)
-  let xs = fmap read . lines $ contents
-  print . solve1 $ xs
+  let nums   = fmap read . lines $ contents
+      sorted = 0 : (sort nums ++ [maximum nums + 3])
+  print . solve1 $ sorted
+  print . solve2 $ sorted
 
 solve1 :: [Int] -> Int
 solve1 xs = m Map.! 1 * m Map.! 3 where
-  max   = maximum xs + 3
-  xs'   = 0 : (sort $ max : xs)
-  diffs = zipWith (\x y -> y - x) xs' (tail xs' ++ [max])
+  diffs = zipWith (\x y -> y - x) xs (tail xs)
   m     = foldr (\x m -> Map.insertWith (+) x 1 m) Map.empty diffs
+
+
+solve2 :: [Int] -> Integer
+solve2 []     = 0
+solve2 [x]    = 1
+solve2 (x:xs) = sum (fmap solve2 forks) where
+  forks = takeWhile filterer . tails $ xs
+  filterer :: [Int] -> Bool
+  filterer []     = True
+  filterer (y:ys) = y < x+4
